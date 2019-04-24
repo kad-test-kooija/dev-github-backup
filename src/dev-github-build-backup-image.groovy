344ac32652a2bb0423e5663cdb4d1cfae89768b3
arno-vdk <23665068+arno-vdk@users.noreply.github.com>
2019-04-24 15:17:10 +0200

    Changed indentation.

--- src/dev-github-build-backup-image.groovy
+++ src/dev-github-build-backup-image.groovy
@@ -2,55 +2,55 @@
 
 // Globals
 def buildEnv = [
-		credentialsId:  env.CREDENTIALS_ID,
-		dockerLabel:    env.DOCKER_LABEL,
-		githubImage:    env.GITHUB_IMAGE
+      credentialsId:  env.CREDENTIALS_ID,
+      dockerLabel:    env.DOCKER_LABEL,
+      githubImage:    env.GITHUB_IMAGE
 ]
 
 pipeline {
-	agent any
+  agent any
 
-	options {
-		buildDiscarder( logRotator( numToKeepStr: '10',
-				artifactNumToKeepStr: '5'))
-		timeout(time: 1, unit: 'HOURS')
-	}
+  options {
+    buildDiscarder( logRotator( numToKeepStr: '10',
+      artifactNumToKeepStr: '5'))
+    timeout(time: 1, unit: 'HOURS')
+  }
 
-	stages {
+  stages {
 
-		stage('Init github-backup image build') {
-			steps {
-				script {
-					echo "Docker Build Host: ${env.DOCKER_BUILD_HOST}"
-					echo "Docker Client Version: ${buildEnv['dockerLabel']}"
+    stage('Init github-backup image build') {
+      steps {
+        script {
+          echo "Docker Build Host: ${env.DOCKER_BUILD_HOST}"
+          echo "Docker Client Version: ${buildEnv['dockerLabel']}"
 
-					checkout changelog: false,
-							poll: false,
-							scm: [
-									$class: 'GitSCM',
-									branches: [[name: '*/stable']],
-									doGenerateSubmoduleConfigurations: false,
-									extensions: [],
-									submoduleCfg: [],
-									userRemoteConfigs: [[url: 'https://github.com/github/backup-utils.git']]
-							]
-				}
-			}
-		}
+          checkout changelog: false,
+                   poll: false,
+                   scm: [
+                     $class: 'GitSCM',
+                     branches: [[name: '*/stable']],
+                     doGenerateSubmoduleConfigurations: false,
+                     extensions: [],
+                     submoduleCfg: [],
+                     userRemoteConfigs: [[url: 'https://github.com/github/backup-utils.git']]
+                   ]
+        }
+      }
+    }
 
-		stage('Build github-backup image') {
-			steps {
-				script {
-					docker.withTool(buildEnv['dockerLabel']) {
-						docker.withServer("tcp://${env.DOCKER_BUILD_HOST}:2376", env.DOCKER_BUILD_HOST.tokenize('.')[0]) {
-							buildImage = docker.build(buildEnv['githubImage'], "--build-arg http_proxy=${http_proxy} .")
-							docker.withRegistry("", buildEnv['credentialsId']) {
-								buildImage.push()
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+    stage('Build github-backup image') {
+      steps {
+        script {
+          docker.withTool(buildEnv['dockerLabel']) {
+            docker.withServer("tcp://${env.DOCKER_BUILD_HOST}:2376", env.DOCKER_BUILD_HOST.tokenize('.')[0]) {
+              buildImage = docker.build(buildEnv['githubImage'], "--build-arg http_proxy=${http_proxy} .")
+              docker.withRegistry("", buildEnv['credentialsId']) {
+                buildImage.push()
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
